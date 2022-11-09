c1a25b48c188140f858e1dbd183b858f8c9a0d6b
yashbabiya <yashbabiya18@gmail.com>
2022-11-09 17:32:15 +0530

    scoket

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useState } from 'react'
 import ChatPopup from './ChatPopup';
 import ChatIcon from '@mui/icons-material/Chat';
 
 export default function Chat() {
+  const socket = useRef();
+  const [modelShow,setModeShow] = useState(false);
+  const [contacts, setContacts] = useState([]);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [currentUser, setCurrentUser] = useState(undefined);
 
-    const [modelShow,setModeShow] = useState(false);
+  useEffect(() => {
+    const chatFun = async () => {
+      setCurrentUser(
+        await JSON.parse(
+          localStorage.getItem("chat-app-current-user")
+        )
+      );
+    }
+    chatFun();
+  }, [])
+  
+
   return (
 
     <div className='chat'>
