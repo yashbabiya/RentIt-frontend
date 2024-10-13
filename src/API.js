974d8b3a8b3df0cfc8e9e2d6a74f1c750b344fed
yBabiya <yash.babiya@portpro.io>
2024-10-13 12:47:39 +0530

    changes

--- src/API.js
+++ src/API.js
@@ -1,2 +1,2 @@
-export const API = "http://localhost:5001/api"
-export const host = "http://localhost:5001";
+export const API = process.env.REACT_APP_API_URL+'/api' || "http://localhost:5001/api"
+export const host = process.env.REACT_APP_API_URL || "http://localhost:5001";
